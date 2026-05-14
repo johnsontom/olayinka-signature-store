f914bb8845a761949e7ddb0dfe006d29997529c5
Johnson Tomiwa <jtomiwa80@gmail.com>
2026-05-14 21:18:23 +0100

    Fix sitemap domain

--- olayinka-signature/src/app/sitemap.ts
+++ olayinka-signature/src/app/sitemap.ts
@@ -3,12 +3,12 @@ import { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://olayinka-signature-store.vercel.app",
+      url: "https://olayinkasignature.vercel.app",
       lastModified: new Date(),
     },
 
     {
-      url: "https://olayinka-signature-store.vercel.app/collection",
+      url: "https://olayinkasignature.vercel.app/collection",
       lastModified: new Date(),
     },
   ]
